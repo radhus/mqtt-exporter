424354c33f3bc5cbf1a3bfd15fb9f82def1eb061
William Johansson <radar@radhuset.org>
2020-11-14 19:18:47 +0100

    Support multiple -topic-pattern flags

--- main.go
+++ main.go
@@ -15,11 +15,22 @@ import (
 
 const topicLabel = "topic"
 
+type stringList []string
+
+func (sl *stringList) String() string {
+	return ""
+}
+
+func (sl *stringList) Set(s string) error {
+	*sl = append(*sl, s)
+	return nil
+}
+
 var (
-	url          = flag.String("url", "", "MQTT URL")
-	clientID     = flag.String("client-id", "mqtt-exporter", "MQTT client ID")
-	topicPattern = flag.String("topic-pattern", "", "Subscribe to topic pattern")
-	listenAddr   = flag.String("addr", ":8080", "HTTP server listening address")
+	url           = flag.String("url", "", "MQTT URL")
+	clientID      = flag.String("client-id", "mqtt-exporter", "MQTT client ID")
+	topicPatterns = stringList{}
+	listenAddr    = flag.String("addr", ":8080", "HTTP server listening address")
 
 	topicGauge = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
@@ -31,8 +42,9 @@ var (
 )
 
 func main() {
+	flag.Var(&topicPatterns, "topic-pattern", "Subscribe to topic pattern (can be repeated)")
 	flag.Parse()
-	if *url == "" || *topicPattern == "" {
+	if *url == "" || len(topicPatterns) == 0 || (len(topicPatterns) == 1 && topicPatterns[0] == "") {
 		fmt.Println("-url and -topic-pattern are both required")
 		flag.Usage()
 		os.Exit(1)
@@ -54,9 +66,11 @@ func main() {
 		log.Fatalln("Failed to connect to MQTT:", token.Error())
 	}
 
-	if token := client.Subscribe(*topicPattern, 0, messageHandler); token.Wait() && token.Error() != nil {
-		client.Disconnect(0)
-		log.Fatalln("Failed to subscribe to MQTT:", token.Error())
+	for _, topicPattern := range topicPatterns {
+		if token := client.Subscribe(topicPattern, 0, messageHandler); token.Wait() && token.Error() != nil {
+			client.Disconnect(0)
+			log.Fatalln("Failed to subscribe to MQTT:", token.Error())
+		}
 	}
 
 	log.Println("Listening on:", *listenAddr)
